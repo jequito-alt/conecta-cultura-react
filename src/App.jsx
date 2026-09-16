5c52dca7b2ca7aacef2cc897eae2ff3c99048bc4
AV-Alumno <AV-Alumno@duoc.acad>
2026-09-16 20:29:37 -0300

    feat: iniciar proyecto React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Bienvenida from "./components/Bienvenida";
+import Cabecera from "./components/Cabecera";
+import TarjetaActividad from "./components/TarjetaActividad";
+import Navegacion from "./components/Navegacion";
+
+function App() {
+  return (
+    <>
+      <Cabecera />
+      <Navegacion />
+      <main className="container py-4">
+        <Bienvenida />
+        <div className="row g-4">
+          <div className="col-12 col-md-6 col-lg-4">
+            <TarjetaActividad />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
